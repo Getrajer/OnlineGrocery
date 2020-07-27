bd7c948f5d581efc729dda142078474ea5138fb3
getrajer533 <getrajer533@gmail.com>
2020-07-28 00:27:21 +0100

    Adding more to organizer

--- OnlineGrocery/Controllers/OrganizerController.cs
+++ OnlineGrocery/Controllers/OrganizerController.cs
@@ -2,12 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using OnlineGrocery.Models;
 
 namespace OnlineGrocery.Controllers
 {
     public class OrganizerController : Controller
     {
+        private readonly UserManager<UserModel> _userManager;
+        private readonly IChatRepository _chatRepository;
+        private readonly INotesRepository _notesRepository;
+
+        public OrganizerController(UserManager<UserModel> userManager,
+                                    IChatRepository chatRepository,
+                                    INotesRepository notesRepository)
+        {
+            _userManager = userManager;
+            _chatRepository = chatRepository;
+            _notesRepository = notesRepository;
+        }
 
         /// <summary>
         /// This function allows team members to leave notes for everyone
@@ -19,6 +33,17 @@ namespace OnlineGrocery.Controllers
             return View();
         }
 
+        [HttpGet]
+        public IActionResult Chat()
+        {
+            return View();
+        }
 
+
+        [HttpGet]
+        public IActionResult MessagesInbox()
+        {
+            return View();
+        }
     }
 }
